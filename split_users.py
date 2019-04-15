2b4746f39d31d45c9d94bbfacadff2acdf559dc6
rudyarthur <rudy.d.arthur@gmail.com>
2019-04-15 16:22:59 +0100

    bugfix

--- split_users.py
+++ split_users.py
@@ -37,7 +37,7 @@ with open(tweet_file,'r') as datafile:
 	
 		
 		##skip big boxes
-		if (words[2][2][0] - words[2][0][0]) < max_place and (words[2][2][1] - words[2][0][1]) < max_place:
+		if not( (words[2][2][0] - words[2][0][0]) < max_place and (words[2][2][1] - words[2][0][1]) < max_place ):
 			skipped += 1;
 			continue;
 
