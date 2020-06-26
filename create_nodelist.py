fb98f6b844aa1316363c511684758f41620bb284
rishanshah <rishanshah@hotmail.co.uk>
2020-06-26 13:14:45 +0100

    edit to nodelistcreation

--- create_nodelist.py
+++ create_nodelist.py
@@ -11,8 +11,9 @@ import pandas as pd
 f = open("communities.out")
 k = json.loads(f.read())
 
-data = pd.DataFrame(k.values(),index = k.keys())
-
+data = pd.DataFrame(k.values(),index = k.keys()).reset_index()
+data.columns = ['Id','Label']
+data.set_index('Id',inplace = True)
 data.to_csv("nodelist.csv")
 
 
